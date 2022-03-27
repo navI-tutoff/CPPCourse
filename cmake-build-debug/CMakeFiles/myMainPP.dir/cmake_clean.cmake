f45806e5f3b8c6c99f61d99599cbeaea8a1cc73b
navI-tutoff <tutov.danil1234@gmail.com>
2022-03-27 13:15:17 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/myMainPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myMainPP.dir/main.cpp.obj"
+  "libmyMainPP.dll.a"
+  "myMainPP.exe"
+  "myMainPP.exe.manifest"
+  "myMainPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myMainPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
