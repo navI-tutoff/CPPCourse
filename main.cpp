74f1f95eba42d5ae06b7abd9ca46627b5d50ade0
navI-tutoff <tutov.danil1234@gmail.com>
2022-05-11 02:56:05 +0300

    ninth exercise (prev. was 10th) from practise bop

--- main.cpp
+++ main.cpp
@@ -1,25 +1,81 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
+#include <cassert>
+
+template<typename VT, typename CI>
+void inputMatrix(VT &m, CI nRows, CI nCols) {
+    for (int i = 0; i < nRows; i++) {
+        std::vector<int> vectorCols(nCols);
+        for (int j = 0; j < nCols; j++) {
+            std::cin >> vectorCols.at(j);
+        }
+        m.push_back(vectorCols);
+    }
+}
+
+template<typename VT, typename CI>
+void outputMatrix(VT &m, CI nRows, CI nCols) {
+    for (int i = 0; i < nRows; i++) {
+        for (int j = 0; j < nCols; j++) {
+            std::cout << m.at(i).at(j) << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
+std::vector<std::vector<int>> transposeMatrix(std::vector<std::vector<int>> &m) {
+    int nRows = m.size();
+    int nCols = m.at(0).size();
+    std::vector<std::vector<int>> newMatrix(nCols, std::vector<int>(nRows)); // transpose
+    for (int i = 0; i < nRows; i++) {
+        for (int j = 0; j < nCols; j++) {
+            newMatrix.at(j).at(i) = m.at(i).at(j);
+        }
+    }
+
+    return newMatrix;
+}
+
+std::vector<std::vector<int>> multiplyMatrices(std::vector<std::vector<int>> &m1,
+                                               std::vector<std::vector<int>> &m2) {
+    int m1NRows = m1.size();
+    int m1NCols = m1.at(0).size();
+    int m2NCols = m2.at(0).size();
+    std::vector<std::vector<int>> newMatrix(m1NRows, std::vector<int>(m2NCols));
+
+    for (int i = 0; i < m1NRows; i++)  {
+        for (int j = 0; j < m2NCols; j++) {
+            for (int k = 0; k < m1NCols; k++) {
+                newMatrix.at(i).at(j) += m1.at(i).at(k) * m2.at(k).at(j);
+            }
+        }
+    }
+
+    return newMatrix;
+}
+
+std::vector<std::vector<int>> calculateValueOfTheQuadraticForm(std::vector<std::vector<int>> &m1,
+                                                               std::vector<std::vector<int>> &m2) {
+    std::vector<std::vector<int>> tm1 = transposeMatrix(m1);
+    std::vector<std::vector<int>> firstMove = multiplyMatrices(tm1, m2);
+
+    return multiplyMatrices(firstMove, m1);
+}
 
 int main() {
-    int stringsNumber;
-    std::cout << "Enter the number of lines ot text: ";
-    std::cin >> stringsNumber;
+    int nRows, nCols;
+    std::cin >> nRows >> nCols;
 
-    std::vector<std::string> text(stringsNumber);
-    size_t maxLength = 0;
-    std::string line;
-    for (int i = 0; i < stringsNumber; i++) {
-        std::getline(std::cin, line);
-        text.at(i) = line;
-        maxLength = std::max(maxLength, line.size());
-    }
 
-    for (auto &line: text) {
-        std::cout << std::setw((maxLength + line.size() / 2));
-        std::cout << std::right << line << '\n';
-    }
+    std::vector<std::vector<int>> vector_x;
+    inputMatrix(vector_x, nRows, 1);
+
+    std::vector<std::vector<int>> matrix;
+    inputMatrix(matrix, nRows, nCols);
+
+    std::vector<std::vector<int>> resMatrix = calculateValueOfTheQuadraticForm(vector_x, matrix);
+
+    outputMatrix(resMatrix, 1, 1);
 
     return 0;
 }
