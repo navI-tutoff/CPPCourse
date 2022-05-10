62de4d0ace8aa713714323b4c83bffb5e468bd6f
navI-tutoff <tutov.danil1234@gmail.com>
2022-05-10 21:54:35 +0300

    seventh exercise from practise bop

--- main.cpp
+++ main.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::string z;
-    std::cin >> z;
-
-    int counter = 1;
-    int maxLen = 0;
-    for (int i = 0; i < z.size() - 1; i++) {
-        if (z.at(i) != z.at(i + 1)) {
-            counter++;
+bool isDivisibleByEachOfItsDigits(const int n) {
+    int copyN = n;
+    bool flag = true;
+    while (copyN && flag) {
+        if (copyN % 10 == 0 || copyN % (copyN % 10) != 0) {
+            flag = false;
         } else {
-            maxLen = counter;
-            counter = 1;
+            copyN /= 10;
         }
     }
 
-    maxLen = std::max(maxLen, counter);
-    std::cout << maxLen;
+    return flag;
+}
+
+// returns vector with all numbers with divisible by each of its digits
+std::vector<int> getAllNumbersWithDivisibleByEachOfItsDigits(const int n) {
+    std::vector<int> res;
+    for (int i = 1; i <= n; i++) {
+        if (isDivisibleByEachOfItsDigits(i)) {
+            res.push_back(i);
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    auto res = getAllNumbersWithDivisibleByEachOfItsDigits(n);
+    for (auto &x: res) {
+        std::cout << x << ' ';
+    }
 
     return 0;
 }
